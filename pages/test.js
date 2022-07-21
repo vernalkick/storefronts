0117079cf2cad56062de40dcbf74d6fd442f848c
Kevin Clark <kevin@kevinclark.ca>
2022-07-21 17:53:19 -0400

    Design updates

--- pages/test.js
+++ pages/test.js
@@ -1,11 +1,13 @@
 
+import styles from '../styles/styles.module.css'
 
 export default function Home() {
   var test = `
   <style>
     model-viewer {
-      width: 100%;
-      height: 500px;  
+      width: calc(100vw - 2rem);
+      height: 100vw;
+      margin-top: -35vw;
     }
   </style>
   
@@ -13,14 +15,22 @@ export default function Home() {
   <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
   
   <!-- Use it like any other HTML element -->
-  <model-viewer alt="Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum" src="/logo.glb" ar ar-modes="webxr scene-viewer quick-look" environment-image="/music_hall_01_1k.hdr" poster="https://modelviewer.dev/shared-assets/models/NeilArmstrong.webp" seamless-poster shadow-intensity="1" camera-controls enable-pan camera-orbit="-1.758deg 92.23deg auto" auto-rotate></model-viewer>`
+  <model-viewer src="/logo.glb" ar ar-modes="webxr scene-viewer quick-look" environment-image="/music_hall_01_1k.hdr" seamless-poster shadow-intensity="1" camera-controls enable-pan camera-orbit="-1.758deg 92.23deg auto" auto-rotate></model-viewer>`
   
   return (
-    <>
-    <img src="ty.svg" />
-    <h1>Thank you for being a part of the team</h1>
-    <div dangerouslySetInnerHTML={{ __html: test }} />
-    </>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Storefronts</h1>
+      <img src="ty.svg" className={styles.ty} />
+      
+      <div dangerouslySetInnerHTML={{ __html: test }} />
+      <div className={styles.text}>
+        <p>Thank you for making it possible for our merchants to build a home on the internet that they can be proud of.</p> 
+        
+        <p>Keep up the great work — Storefronts wouldn’t be the same without you!</p>
+        
+        <p className={styles.signature}>– Vanessa, Rodney, Brad, Beth, Kevin, Phil</p>
+      </div>
+    </div>
 
   )
 }
